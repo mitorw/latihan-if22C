836e6cedba12efc6a96bb0e725f10c3af8ca44d2
mitorahmandhanawijaya <mitorahmandhanawijaya@gmail.com>
2024-12-12 02:57:10 +0000

    terbaru

--- lib/app/modules/dosen/views/dosen_view.dart
+++ lib/app/modules/dosen/views/dosen_view.dart
@@ -1,73 +1,89 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
 import 'package:get/get.dart';
-import 'package:myapp/app/modules/dosen/controllers/dosen_controller.dart';
 import 'package:myapp/app/modules/dosen/views/dosen_update_view.dart';
 
+import '../controllers/dosen_controller.dart';
+
 class DosenView extends GetView<DosenController> {
-  void Showoption(id) async {
-    var result = await Get.dialog(SimpleDialog(
-      children: [
-        ListTile(
-          onTap: () {
-            Get.back();
-            Get.to(
-              DosenUpdateView(),
-              arguments: id,
-            );
-          },
-          title: Text("update"),
+  void ShowOption(id) async {
+    var result = await Get.dialog(
+        SimpleDialog(
+          children: [
+            ListTile(
+              onTap: () {
+                Get.back();
+                Get.to(() => DosenUpdateView(), arguments: id);
+              },
+              title: Text("Update"),
+            ),
+            ListTile(
+              onTap: () {
+                Get.back();
+                controller.delete(id);
+              },
+              title: Text("Delete"),
+            ),
+            ListTile(
+              onTap: () {
+                Get.back();
+              },
+              title: Text("Close"),
+            ),
+          ],
         ),
-        ListTile(
-          onTap: () {
-            Get.back();
-            controller.delete(id);
-          },
-          title: Text("delete"),
-        ),
-        ListTile(
-          onTap: () {
-            Get.back();
-          },
-          title: Text("close"),
-        )
-      ],
-    ));
+        barrierDismissible: false,
+
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot<Object?>>(
-        stream: Get.put(DosenController().streamData()),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.active) {
-            var listAllDocs = snapshot.data?.docs ?? [];
-            return listAllDocs.length > 0
-                ? ListView.builder(
-                    itemCount: listAllDocs.length,
-                    itemBuilder: (context, index) => ListTile(
-                          leading: CircleAvatar(
-                            child: Text('${index + 1}'),
-                            backgroundColor: Colors.white60,
-                          ),
-                          title: Text(
-                              "${(listAllDocs[index].data() as Map<String, dynamic>)["nama"]}"),
-                          subtitle: Text(
-                            "${(listAllDocs[index].data() as Map<String, dynamic>)["nidn"]}",
-                          ),
-                          trailing: IconButton(
-                              onPressed: () {
-                                Showoption(listAllDocs[index].id);
-                              },
-                              icon: Icon(Icons.add_circle_outline)),
-                        ))
-                : Center(
-                    child: Text("data kosong"),
-                  );
-          }
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        });
+      stream: Get.put(DosenController()).streamData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          // ambil data dari firebase
+          var listAllDocs = snapshot.data?.docs ?? [];
+          return listAllDocs.length > 0
+              ? ListView.separated(
+                    separatorBuilder: (context, index) => Divider(
+                      thickness: 1,
+                      color: Colors.grey.shade300,
+                    ),
+                  itemCount: listAllDocs.length,
+                  itemBuilder: (context, index) => ListTile(
+                    leading: CircleAvatar(
+                      child: Text('${index + 1}'),
+                      backgroundColor: Colors.white,
+                    ),
+                    title: Text(
+                        "${(listAllDocs[index].data() as Map<String, dynamic>)["nama"]}"),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("${(listAllDocs[index].data() as Map<String, dynamic>)["nidn"]}"),
+                          Text("${(listAllDocs[index].data() as Map<String, dynamic>)["prodi"]}"),
+                        ],
+                      ),
+                    trailing: IconButton(
+                      onPressed: () {
+                        ShowOption(listAllDocs[index].id);
+                      },
+                      icon: Icon(Icons.more_vert),
+                    ),
+                  ),
+                )
+              : Center(
+                  child: Text("Data Kosong"),
+                );
+        }
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
   }
 }
--- lib/app/modules/mahasiswa/views/mahasiswa_view.dart
+++ lib/app/modules/mahasiswa/views/mahasiswa_view.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import 'package:get/get.dart';
 import 'package:myapp/app/modules/mahasiswa/views/mahasiswa_update_view.dart';
@@ -7,69 +8,73 @@ import 'package:myapp/app/modules/mahasiswa/views/mahasiswa_update_view.dart';
 import '../controllers/mahasiswa_controller.dart';
 
 class MahasiswaView extends GetView<MahasiswaController> {
-  void Showoption(id) async {
-    var result = await Get.dialog(SimpleDialog(
-      children: [
-        ListTile(
-          onTap: () {
-            Get.back();
-            Get.to(
-              MahasiswaUpdateView(),
-              arguments: id,
-            );
-          },
-          title: Text("update"),
+  void ShowOption(id) async {
+    var result = await Get.dialog(
+        SimpleDialog(
+          children: [
+            ListTile(
+              onTap: () {
+                Get.back();
+                Get.to(() => MahasiswaUpdateView(), arguments: id);
+              },
+              title: Text("Update"),
+            ),
+            ListTile(
+              onTap: () {
+                Get.back();
+                controller.delete(id);
+              },
+              title: Text("Delete"),
+            ),
+            ListTile(
+              onTap: () {
+                Get.back();
+              },
+              title: Text("Close"),
+            ),
+          ],
         ),
-        ListTile(
-          onTap: () {
-            Get.back();
-            controller.delete(id);
-          },
-          title: Text("delete"),
-        ),
-        ListTile(
-          onTap: () {
-            Get.back();
-          },
-          title: Text("close"),
-        )
-      ],
-    ));
+        barrierDismissible: false,
+
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot<Object?>>(
-        stream: Get.put(MahasiswaController().StreamData()),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.active) {
-            var listAllDocs = snapshot.data?.docs ?? [];
-            return listAllDocs.length > 0
-                ? ListView.builder(
-                    itemCount: listAllDocs.length,
-                    itemBuilder: (context, index) => ListTile(
-                          leading: CircleAvatar(
-                            child: Text('${index + 1}'),
-                            backgroundColor: Colors.white60,
-                          ),
-                          title: Text(
-                              "${(listAllDocs[index].data() as Map<String, dynamic>)["nama"]}"),
-                          subtitle: Text(
-                            "${(listAllDocs[index].data() as Map<String, dynamic>)["npm"]}",
-                          ),
-                          trailing: IconButton(
-                              onPressed: () {
-                                Showoption(listAllDocs[index].id);
-                              },
-                              icon: Icon(Icons.add_circle_outline)),
-                        ))
-                : Center(
-                    child: Text("data kosong"),
-                  );
-          }
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        });
+      stream: Get.put(MahasiswaController()).StreamData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          // ambil data dari firebase
+          var listAllDocs = snapshot.data?.docs ?? [];
+          return listAllDocs.length > 0
+              ? ListView.builder(
+                  itemCount: listAllDocs.length,
+                  itemBuilder: (context, index) => ListTile(
+                    leading: CircleAvatar(
+                      child: Text('${index + 1}'),
+                      backgroundColor: Colors.white,
+                    ),
+                    title: Text(
+                        "${(listAllDocs[index].data() as Map<String, dynamic>)["nama"]}"),
+                    subtitle: Text(
+                        "${(listAllDocs[index].data() as Map<String, dynamic>)["npm"]}"),
+                    trailing: IconButton(
+                      onPressed: () {
+                        ShowOption(listAllDocs[index].id);
+                      },
+                      icon: Icon(Icons.more_vert),
+                    ),
+                  ),
+                )
+              : Center(
+                  child: Text("Data Kosong"),
+                );
+        }
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
   }
 }
